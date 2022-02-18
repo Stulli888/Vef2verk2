2a0bfba2c437c91a4cd9118fa98e0b4622a9ce05
Sturla <sfm1@hi.is>
2022-02-18 22:47:49 +0000

    undo last change

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.set('views', join(path, '../views'));
 app.set('view engine', 'ejs');
 
 app.use(session({
-  secret: SESSION_SECRET,
+  secret: sessionSecret,
   resave: false,
   saveUninitialized: false,
   maxAge: 20 * 1000, // 20 sek
